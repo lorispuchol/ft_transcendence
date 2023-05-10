865d74b54f51bce8ba7830eaec88d9591241002f
salty-kambi <luc.suau@gmail.com>
2023-05-10 14:19:10 +0200

    nest project

--- clean.sh
+++ clean.sh
@@ -5,5 +5,5 @@ docker stop $(docker ps -aq)
 docker rm -fv $(docker ps -aq)
 docker rmi -f $(docker images -qa)
 docker volume rm -f $(docker volume ls -q)
-docker network rm -f $(docker network ls -q)
+#docker network rm -f $(docker network ls -q)
 echo "Done"
