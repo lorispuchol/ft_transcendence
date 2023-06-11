63b6dbc273b2c218638b4c389a4c319d124d6df1
salty-kambi <luc.suau@gmail.com>
2023-06-11 15:24:00 +0200

    front test + few change on docker compose

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,12 +14,36 @@ function App() {
 	const user = {
 		name: 'loris',
 		imageUrl: 'https://cdn.intra.42.fr/users/2eb074494d7888e1d6cabd69dea349d0/lpuchol.jpg',
-		imageSize: 90,
+		imageSize: 250,
 	};
+	const bouton = <MyButton />;
+	const products = [
+		{ title: 'Cabbage', id: 1 },
+		{ title: 'Garlic', id: 2 },
+		{ title: 'Apple', id: 3 },
+	];
 
   return (
 	<div>
-		<h1>
+		<a
+			href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfJ8vUgJfEaBtkAd3yWLXpZb2okK11ZtfxvDj0Thc&usqp=CAE&s"
+			title="big floppa"
+			target="_blank"
+		>
+			<p className="desc">Mi <strong>gato</strong></p>
+		</a>
+		<img
+			className="flopa"
+			src="https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg"
+			alt="flopa"
+			style={{
+				width: user.imageSize + 50,
+				height: user.imageSize
+			}}
+		/>
+		<input type="text" disabled />
+		<input type="text" />
+		{/* <h1>
 			{user.name}
 		</h1>
 		<img 
@@ -32,7 +56,7 @@ function App() {
 			}}
 		/>
 		<h1>Welcome</h1>
-		<MyButton />
+		{bouton} */}
    </div>
   );
 }
