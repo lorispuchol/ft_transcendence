b871dd6a089697a28bd8aaa3084f705206676ec0
salty-kambi <luc.suau@gmail.com>
2023-06-25 12:59:51 +0200

    user route fix + navbar

--- client/src/home/Home.tsx
+++ client/src/home/Home.tsx
@@ -1,15 +1,15 @@
 
 import { Link, Navigate } from "react-router-dom"
+import ResponsiveAppBar from "./NavBar";
 
 export default function Home() {
 	if (!localStorage.getItem("token"))
 	 	return (<Navigate to='/login' />);
 	return (
 		<>
-			home
-			<Link to="/profile">
-				<button>profile</button>
-			</Link>
+			<ResponsiveAppBar />
+			<div><strong><u>home</u></strong></div>
+			<div><Link to="/profile"><button>profile</button></Link></div>
 		</>
 	);
 }
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -11,29 +11,46 @@ export class UserController {
 		private readonly friendshipService: FriendshipService,
 		private userService: UserService
 	) {}
-
+	
+	
 	@Get('me')
 	async getMeData(
 		@Request() req: any
-	) {
+		) {
 		return await this.userService.findOneByUsername(req.username);
 	}
-
-	@Get(':username')
-	async getUserData(
-		@Param('username') username: string
-	) {
-		return await this.userService.findOneByUsername(username);
-	}
+	
 
 	@Patch('username')
 	async changeUsername(
 		@Request() req: any,
 		@Body('username') newUsername: string
-	) {
+		) {
 		const status = await this.userService.changeUsername(req.user.id, newUsername);
 		const user =  await this.userService.findOneById(req.user.id);
 		return {status: status, user: user};
+	}
+
+	
+	//dev
+	@Public()
+	@Get('all')
+	async getAllUsers() {
+		return await this.userService.getAllUsers();
+	}
+
+	//dev
+	@Public()
+	@Get('allfriendship')
+	async getAllFriendship() {
+		return await this.friendshipService.getAllFriendship();
+	}
+	
+	//dev
+	@Public()
+	@Get('create/:login')
+	createFakeUser(@Param('login') login: string): Promise<User> {
+		return this.userService.createOne(login);
 	}
 
 	@Get('askFriend/:recipient')
@@ -45,24 +62,12 @@ export class UserController {
 				await this.userService.findOneById(recipientId)
 			)
 	}
-
-	//dev/////////////////////////////////////
-	@Public()
-	@Get('all')
-	async getAllUsers() {
-		return await this.userService.getAllUsers();
+ 	
+	@Get(':username')
+	async getUserData(
+		@Param('username') username: string
+		) {
+			return await this.userService.findOneByUsername(username);
 	}
 
-	@Public()
-	@Get('create/:login')
-	createFakeUser(@Param('login') login: string): Promise<User> {
-		return this.userService.createOne(login)
-	}
- 
-	@Public()
-	@Get('allfriendship')
-	async getAllFriendship() {
-		return await this.friendshipService.getAllFriendship();
-	}
-	////////////////////////////////////////////
 }
