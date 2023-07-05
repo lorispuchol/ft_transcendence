55fcadad60abb6c725e5d0b3b909db1c9851f4cc
mxpnt <maxime.pontille1@gmail.com>
2023-07-05 13:13:39 +0200

    convert tailwindcss into scss LogIn page

--- client/src/pages/LogIn/LogIn.tsx
+++ client/src/pages/LogIn/LogIn.tsx
@@ -1,7 +1,6 @@
 import { useSearchParams } from "react-router-dom";
 import { client_url, server_url } from "../../utils/Request";
 import Loading from "../../utils/Loading";
-import { Box } from "@mui/material";
 import './LogIn.scss'
 
 export default function LogIn() {
@@ -13,16 +12,18 @@ export default function LogIn() {
 		window.location.replace(client_url);
 		return (<Loading />);
 	}
+
+	function clickButton() {
+		const url = server_url + "/auth/";
+		window.location.href=url;
+	}
+
 	return (
-		<Box className="box-border justify-center flex flex-col foreground_login w-72 h-96 ring ring-black">
-			<h1 className="flex justify-center text-4xl font-poppins font-bold mb-8">EL PONGO</h1>
-			<div className="flex justify-center">
-				<button className="flex justify-center text-2xl rounded ring ring-black w-24 mb-8">
-					<div className="button_login hover:opacity-60 rounded w-24 transition-opacity duration-300">
-						<a className="font-poppins" href={server_url + "/auth/"} >LOG IN</a>
-					</div>
-				</button>
+		<div className="box_login background_box_login">
+			<div className="title_box">EL PONGO</div>
+			<div className="button_box">
+				<button className="button_login active:scale-110" onClick={clickButton}>LOG IN</button>
 			</div>
-		</Box>
+		</div>
 	);
 }
