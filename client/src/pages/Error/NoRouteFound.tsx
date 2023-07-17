e6554166cdf9496a40beca47d2cacc54055505de
mxpnt <maxime.pontille1@gmail.com>
2023-07-17 10:46:44 +0200

    fix 404 on navbar

--- client/src/pages/Error/NoRouteFound.tsx
+++ client/src/pages/Error/NoRouteFound.tsx
@@ -3,8 +3,8 @@ import './Error.scss'
 
 export default function NoRouteFound() {
 	return (
-		<div className='centered'>
-			<div className="flex flex-col flex-wrap justify-center justify-items-center items-center place-content-center">
+		<div className='centered mt-8'>
+			<div className="flex flex-col flex-wrap justify-center justify-items-center items-center ">
 				<div className='text-9xl'>404</div>
 				<div className='mb-8 text-2xl'>PAGE NOT FOUND</div>
 				<div>
