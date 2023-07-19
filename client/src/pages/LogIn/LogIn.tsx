592b17649a2c3142ac2d96ee8a90139161b4504d
salty-kambi <82607700+salty-kambi@users.noreply.github.com>
2023-07-19 18:33:40 +0200

    Update LogIn.tsx

--- client/src/pages/LogIn/LogIn.tsx
+++ client/src/pages/LogIn/LogIn.tsx
@@ -50,25 +50,4 @@ export default function LogIn() {
 			</div>
 		</div>
 	);
-
-	// return (
-	// 	<Box className="box-border justify-center flex flex-col foreground_login w-80 h-110 ring ring-black">
-	// 		<h1 className="flex justify-center text-4xl font-poppins font-bold mb-8">EL PONGO</h1>
-	// 		<div className="flex justify-center">
-	// 			<button className="button_login flex justify-center text-2xl rounded hover:bg-purple-300 ring ring-black w-32 mb-8">
-	// 				<a className="font-poppins font-bold" href={server_url + "/auth/"} >LOG IN WITH 42</a>
-	// 			</button>
-	// 		</div>
-
-	// 		<h2 className="flex justify-center text-2xl font-poppins font-bold mb-8 mt-10">Or with username</h2>
-	// 		<div className="flex justify-center">
-	// 		<form>
-	// 			<input  type="text"  value={username} onChange={handleChange} />
-	// 		</form>
-	// 			<button className="button_login flex justify-center text-2xl rounded hover:bg-purple-300 ring ring-black w-32 mb-8">
-	// 				<a className="font-poppins font-bold" href={server_url + "/auth/loginByUsername/" + username} >Send</a>
-	// 			</button>
-	// 		</div>
-	// 	</Box>
-	// );
 }
