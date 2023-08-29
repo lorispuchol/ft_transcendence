479582e564d820abe97937035e6dc052590ad8a6
salty-kambi <luc.suau@gmail.com>
2023-08-29 20:05:55 +0200

    pong start anim

--- client/src/pages/game/Game.tsx
+++ client/src/pages/game/Game.tsx
@@ -4,6 +4,7 @@ import "./Game.scss";
 import handlePaddle, { Pad, handleKey, init_paddle } from "./paddle";
 import handleBall, { Ball, drawBall, init_ball } from "./ball";
 import collision from "./collision";
+import { countDown } from "./countDown";
 
 let freezeFrame = 0;
 
@@ -14,19 +15,9 @@ function checkPoint(ctx: CanvasRenderingContext2D, width: number, height: number
 		freezeFrame = 20;
 		Object.assign(paddle, init_paddle(width,height));
 		Object.assign(ball, init_ball(width, height));
+		return true;
 	}
-}
-
-const times: number[] = [];
-function displayFps(ctx: CanvasRenderingContext2D, timestamp: number) {
-	const now = timestamp;
-	while (times.length > 0 && times[0] <= now - 1000) {
-		times.shift();
-	}
-	times.push(now);
-	const fps = times.length;
-	ctx.font = "50px Arial"
-	ctx.fillText(fps + " fps", 40, 70);
+	return false;
 }
 
 export default function Game() {
@@ -34,25 +25,35 @@ export default function Game() {
 	
 	useEffect(() => {
 		const ctx = canvasRef!.current!.getContext('2d')!;
-		let animationFrameId: number;
-
-		const width = ctx.canvas.width = 3200
+		
+		const width = ctx.canvas.width = 3200;
 		const height = ctx.canvas.height = 1800;
 		const [idKey] = handleKey();
 		const ball : Ball = init_ball(width, height);
 		const paddle: Pad = init_paddle(width, height);
-
+		
+		let animationFrameId: number;
+		let roundStart: boolean = true;
+		let now = performance.now();
 
 		function render(timestamp: number) {
 			if (freezeFrame)
+			{
 				freezeFrame--;
+				now = performance.now();
+			}
+			else if (roundStart)
+			{
+				roundStart = countDown(ctx, now, 500);
+				handlePaddle(ctx, paddle);
+				drawBall(ctx, ball);
+			}
 			else {
 				ctx.clearRect(0,0, width, height);
-				displayFps(ctx, timestamp);
 				handlePaddle(ctx, paddle);
 				collision(width, height, paddle, ball);
 				handleBall(ctx, ball);
-				checkPoint(ctx, width, height, paddle, ball);
+				roundStart = checkPoint(ctx, width, height, paddle, ball);
 			}
 			animationFrameId = window.requestAnimationFrame(render);
 		}
